3b1928375b5baf30dba9f9a158cc18e91c5f12ff
luccunha <lucas.cunha@capgemini.com>
2023-04-24 18:35:36 -0300

    Adding tests

---
+++ GeekBurger.Products.UnitTests/Controllers/ProductsControllerUnitTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using GeekBurger.Products.Controllers;
+using GeekBurger.Products.Model;
+using GeekBurger.Products.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBurger.Products.UnitTests.Controllers
+{
+
+    public class ProductsControllerUnitTests
+    {
+        private readonly ProductsController _productsController;
+        private Mock<IProductsRepository> _productRepositoryMock;
+        private Mock<IMapper> _mapperMock;
+
+        public ProductsControllerUnitTests()
+        {
+            _productRepositoryMock = new Mock<IProductsRepository>();
+            _mapperMock = new Mock<IMapper>();
+            _productsController = new ProductsController(_productRepositoryMock.Object, _mapperMock.Object);
+        }
+
+        [Fact]
+        public void OnGetProductsByStoreName()
+        {
+            //arrange
+            var storeName = "Paulista";
+            var productList = new List<Product>();
+            _productRepositoryMock.Setup(_ => _.GetProductsByStoreName(storeName)).Returns(productList);
+            var expected = new NotFoundObjectResult("Nenhum dado encontrado");
+
+            //act
+            var response = _productsController.GetProductsByStoreName(storeName);
+
+            //assert            
+            Assert.IsType<NotFoundObjectResult>(response);
+            response.Should().BeEquivalentTo(expected);
+        }
+    }
+}
